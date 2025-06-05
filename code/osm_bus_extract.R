d2af5f9fbb37362e70e11b36c4e2492754174aa6
RFlx <temospena@gmail.com>
2025-06-05 15:57:09 +0100

    improve algorythm
    #4

--- code/osm_bus_extract.R
+++ code/osm_bus_extract.R
@@ -39,6 +39,17 @@ mapview(carris_osm_platform, zcol = "ref")
 # st_write(carris_osm_lines, "data/carris_osm_lines.gpkg", delete_dsn = TRUE)
 
 
+# retrieve info from gtfs
+
+gtfs_carris = load_feed(data_sources$producer_url[1])
+next_wednesday = calendar_nextBusinessWednesday(country_code="PT")
+gtfs_carris = tidytransit::filter_feed_by_date(gtfs_carris, extract_date = next_wednesday)
+# gtfs_carris = gtfs_carris |> filter_by_modes(modes = list(0, 3))  # filter by mode = tram and bus
+routes_freq_lisbon_hour_no_overline = GTFShift::get_route_frequency_hourly(gtfs = gtfs_carris,
+                                                                           date = next_wednesday,
+                                                                           overline = FALSE)
+
+
 # test with 1 route -------------------------------------------------------
 
 carris_osm_735 = carris_osm_multilines_redux |> 
@@ -58,15 +69,7 @@ carris_osm_735_geom = carris_osm_735_geom |>
   select(idseq, initial, final, route_dist, geometry) |> 
   arrange(route_dist, initial, final)
 
-# let's find which one corresponds to the shape_id
 
-gtfs_carris = load_feed(data_sources$producer_url[1])
-next_wednesday = calendar_nextBusinessWednesday(country_code="PT")
-gtfs_carris = tidytransit::filter_feed_by_date(gtfs_carris, extract_date = next_wednesday)
-# gtfs_carris = gtfs_carris |> filter_by_modes(modes = list(0, 3))  # filter by mode = tram and bus
-routes_freq_lisbon_hour_no_overline = GTFShift::get_route_frequency_hourly(gtfs = gtfs_carris,
-                                                                           date = next_wednesday,
-                                                                           overline = FALSE)
 # 1. filter by ref / short_name
 carris_gtfs_735 = routes_freq_lisbon_hour_no_overline |> 
   filter(route_short_name == "735") |> 
@@ -111,50 +114,13 @@ carris_gtfs_735_result = carris_gtfs_735_result |>
 
 mapview(carris_gtfs_735_result, zcol = "shape_id")
 
-# 1
 
 
 # mapview(carris_osm_735_geom$initial) + mapview(carris_gtfs_735$initial, col.regions = "red")
 # mapview(carris_osm_735_geom$final) + mapview(carris_gtfs_735$final, col.regions = "red")
 
-distances_start = st_distance(carris_osm_735_geom$initial, carris_gtfs_735$initial)[,1] |> 
-  units::drop_units()
 
 
-
-
-
-
-
-
-
-
-# try with the first shape
-carris_gtfs_735_i = carris_gtfs_735 |> slice(1)
-carris_gtfs_735_i = carris_gtfs_735_i |> 
-  mutate(initial = lwgeom::st_startpoint(carris_gtfs_735_i) |> 
-           st_as_sf(),
-         final = lwgeom::st_endpoint(carris_gtfs_735_i) |> 
-           st_as_sf())
-
-carris_gtfs_735_i_pointinit = carris_gtfs_735_i$initial |> 
-  st_as_sf()
-carris_osm_735_pointinit = carris_osm_735$initial |> 
-  st_as_sf()
-mapview(carris_gtfs_735_i_pointinit) + mapview(carris_osm_735_pointinit, col.regions = "red")
-
-
-
-
-
-# remove units
-  mutate(distance = st_distance(carris_osm_735_pointinit, carris_gtfs_735_i_pointinit)[,1] |>  # in meters
-           units::drop_units()) # remove units
-#ver mqat!
-# https://github.com/U-Shift/EITcourse/blob/fc8a3424b1c3e8ddb8c46f522d122d708875380c/distances.qmd#L96
-# filter(min(distance))
-# encontrar!
-  
 
 
 # Procedure ---------------------------------------------------------------
@@ -180,4 +146,81 @@ mapview(carris_gtfs_735_i_pointinit) + mapview(carris_osm_735_pointinit, col.reg
 # 
 # 6. From the unique identified result that can be assumed as the same between gtfs and osm, assign the geometry to (a new) gtfs shapes.
 # Proceed with the left_joins and ovelines to get the sum of frequency.
+
+
+
+
+# test with 1 route and 6 variants! -------------------------------------------------------
+carreira = "736"
+carris_osm_carreira = carris_osm_multilines_redux |> 
+  filter(ref == carreira)
+nrow(carris_osm_carreira) # 4
+# mapview(carris_osm_carreira)
+
+
+carris_osm_carreira_geom = carris_osm_carreira |> st_line_merge() # from MUTLILINESTRING to LINESTRING
+# mapview(carris_osm_carreira)
+carris_osm_carreira_geom = carris_osm_carreira_geom |> 
+  mutate(route_dist = st_length(carris_osm_carreira_geom) |> units::drop_units()) |> 
+  mutate(initial = lwgeom::st_startpoint(carris_osm_carreira_geom) |> 
+           st_as_sf(),
+         final = lwgeom::st_endpoint(carris_osm_carreira_geom) |> 
+           st_as_sf()) |> 
+  mutate(idseq = row_number()) |> 
+  select(idseq, initial, final, route_dist, geometry) |> 
+  arrange(route_dist, initial, final)
+
+
+# 1. filter by ref / short_name
+carris_gtfs_carreira = routes_freq_lisbon_hour_no_overline |> 
+  filter(route_short_name == carreira) |> 
+  select(route_id, shape_id, route_short_name, direction_id, geometry) |>
+  distinct()
+nrow(carris_gtfs_carreira) 
+
+
+# 2. Compare distances
+carris_gtfs_carreira = carris_gtfs_carreira |> 
+  mutate(route_dist = st_length(carris_gtfs_carreira) |> units::drop_units()) |> 
+  mutate(initial = lwgeom::st_startpoint(carris_gtfs_carreira) |> 
+           st_as_sf(),
+         final = lwgeom::st_endpoint(carris_gtfs_carreira) |> 
+           st_as_sf()) |> 
+  arrange(route_dist, initial, final)
+
+init = st_distance(carris_osm_carreira_geom$initial, carris_gtfs_carreira$initial)
+fin = st_distance(carris_osm_carreira_geom$final, carris_gtfs_carreira$final)
+
+
+conjunto = abs(init+fin)
+carris_gtfs_carreira_minimos = carris_gtfs_carreira |> 
+  mutate(osm_idseq = NA)
+
+for (i in 1:nrow(carris_gtfs_carreira_minimos)) {
+  carris_gtfs_carreira_minimos[i,]$osm_idseq = carris_osm_carreira_geom[which.min(conjunto[i,]),]$idseq
+}
+
+carris_gtfs_carreira_result = carris_gtfs_carreira_minimos |>
+  st_drop_geometry() |>  
+  left_join(carris_osm_carreira_geom |> select(idseq, route_dist, geometry),
+            by = c("osm_idseq" = "idseq")) |> 
+  mutate(distance_diff = route_dist.x - route_dist.y)
+max(carris_gtfs_carreira_result$distance_diff)
+
+carris_gtfs_carreira_result = carris_gtfs_carreira_result |> 
+  select(shape_id, direction_id, route_short_name, geometry) |> 
+  st_as_sf()
+
+mapview(carris_gtfs_carreira_result, zcol = "shape_id")
+
+# IT WORKS!!
+
+# after getting the osm shapes fot all unique shape_id (gtfs), bring back the geometry to the
+routes_freq_lisbon_hour_no_overline_OSM |> st_drop_geometry() |> 
+  left_join(carris_gtfs_carreira_result |> 
+              select(shape_id, geometry),
+            by = "shape_id") |>
+  st_as_sf()
+# test
+
 
